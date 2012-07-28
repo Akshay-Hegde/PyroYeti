7a160249e8489ea616e8afceb6345825fb64a472
fordarnold@gmail.com <fordarnold@gmail.com>
2012-07-28 19:26:20 +0300

    Initial Commit: Added Theme Files

---
+++ theme.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_PyroYeti extends Theme
+{
+    public $name			= 'PyroYeti';
+    public $author			= 'Arnold Mwumva Ford';
+    public $author_website	= 'http://fordarnold.wordpress.com/';
+    public $website			= 'https://github.com/pyrosuit/PyroYeti';
+    public $description		= 'A Responsive PyroCMS Theme built on Foundation 3, HTML5, CSS';
+    public $version			= '1.0';
+}
+
+/* End of file theme.php */
